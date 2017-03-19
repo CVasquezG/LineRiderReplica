22332435608bcab10d4b9daf430f7e1c19a86cf1
CVasquezG <camilo11295@gmail.com>
2017-03-19 12:28:23 -0700

    added line script

---
+++ Assets/Line.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour {
+
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D edgeCol;
+}
